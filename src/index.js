16947cf4164de196b637a660a6f313a7210d415d
saikat2050 <saikatsantra2050@gmail.com>
2023-02-25 11:06:11 +0530

    deployment specific

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const compression = require('compression')
 const router = require('./routes') 
 require('./util/db')
 const message = require('./util/message.json')
 const port = process.env.PORT || 4001
 const app = express()
 
+app.use(helmet())
+app.use(cors())
+app.use(compression())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
@@ -13,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(router)
 
 app.use('*', (req, res) => {
-    res.status(404).json({Status: "Error", Message: message.NOTFOUND});
+    return res.status(404).json({Status: "Error", Message: message.NOTFOUND});
   });
 
 app.listen(port);
